7617e9f6d5206761ae62f43bad41ca46e30bda4c
risusefu <risusefu@e12224b2-b8a6-a7d7-3dec-a5a3c91f73d2>
2011-03-26 19:07:35 +0000

    Agregación de carpetas en sql con versiones

---
+++ trunk/sql/2 tablespace/saib.sql
@@ -0,0 +1,8 @@
+﻿-- Tablespace: saib
+
+-- DROP TABLESPACE saib
+
+CREATE TABLESPACE saib
+  OWNER postgres
+  LOCATION 'C:/tablespace/saib';
+COMMENT ON TABLESPACE saib IS 'Espacio de tablas para el sistema SAIB';
---
+++ trunk/sql/4 types/t_validar_usuarios.sql
@@ -0,0 +1,21 @@
+﻿DROP FUNCTION IF EXISTS validar_usuarios(_log_usu TEXT,_pas_usu TEXT,_tip_usu TEXT);
+DROP TYPE IF EXISTS  t_validar_usuarios;
+CREATE TYPE t_validar_usuarios AS
+   (
+	id_usu_adm INTEGER,
+	nom_usu_adm TEXT,
+	ape_usu_adm TEXT,
+	pas_usu_adm TEXT,
+	log_usu_adm TEXT,
+	tel_usu_adm TEXT,
+	id_tip_usu INTEGER
+   );
+ALTER TYPE t_validar_usuarios OWNER TO postgres;
+COMMENT ON TYPE t_validar_usuarios IS '
+NOMBRE: t_validar_usuarios
+TIPO: TIPO
+	
+CREADOR: Luis Raul Marin	
+MODIFICADO: 
+FECHA: 20/03/2011
+';
