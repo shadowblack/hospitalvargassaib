b3e790373ae870b79f4b56dd4784714531e8fc77
risusefu <risusefu@e12224b2-b8a6-a7d7-3dec-a5a3c91f73d2>
2011-03-26 19:05:44 +0000

    Reestructuracion de los objetos de la base de datos

--- trunk/sql/2 tablespace/saib.sql
+++
@@ -1,8 +0,0 @@
-﻿-- Tablespace: saib
-
--- DROP TABLESPACE saib
-
-CREATE TABLESPACE saib
-  OWNER postgres
-  LOCATION 'C:/tablespace/saib';
-COMMENT ON TABLESPACE saib IS 'Espacio de tablas para el sistema SAIB';
