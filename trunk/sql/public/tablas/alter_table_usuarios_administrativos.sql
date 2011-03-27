e9f7399d6bd33ab0eefbf559d5213b722b926913
risusefu <risusefu@e12224b2-b8a6-a7d7-3dec-a5a3c91f73d2>
2011-03-27 00:20:56 +0000

    Eliminación de las carpetas esquema, type y functions para la nueva restructuración en la carpeta public

---
+++ trunk/sql/public/tablas/alter_table_usuarios_administrativos.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE usuarios_administrativos
+	ADD COLUMN fec_reg_usu_adm CHARACTER VARYING(100);
+
+COMMENT ON COLUMN usuarios_administrativos.fec_reg_usu_adm IS 'Fecha de registro de los usuarios'
