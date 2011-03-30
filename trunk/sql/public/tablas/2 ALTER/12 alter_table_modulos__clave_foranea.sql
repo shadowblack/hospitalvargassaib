0ac21681f7121afcd498aecc64076aafb175dbe6
ninja.aoshi <ninja.aoshi@e12224b2-b8a6-a7d7-3dec-a5a3c91f73d2>
2011-03-30 04:11:23 +0000

    Modificación de la base de datos, clave foranea de modulos con tipos usuarios.

---
+++ trunk/sql/public/tablas/2 ALTER/12 alter_table_modulos__clave_foranea.sql
@@ -0,0 +1,5 @@
+﻿ALTER TABLE modulos
+	ADD COLUMN id_tip_usu INTEGER REFERENCES tipos_usuarios(id_tip_usu),	
+	DROP CONSTRAINT modulos_cod_mod_unique,
+	ADD CONSTRAINT modulos_cod_mod_unique UNIQUE(cod_mod,id_tip_usu);
+	
