8aac7787c2c8e1018778dcca727a7a1f194504e9
moisevicenteperez <moisevicenteperez@e12224b2-b8a6-a7d7-3dec-a5a3c91f73d2>
2011-05-24 23:11:52 +0000

    creacion del medico menu coontroler

---
+++ trunk/app/controllers/medico_menu_controller.php
@@ -0,0 +1,25 @@
+<?php
+    class MedicoMenuController extends Controller{
+        var $name = "MedicoMenu";
+        var $uses = Array();
+       
+        function index(){   
+            
+        }
+   
+        function registrar(){   
+            
+        }
+        function configurar(){   
+            
+        }
+        
+        function reporte(){   
+            
+        }
+        
+        function estadistica(){   
+            
+        }
+    }
+?>
