bad37f980552a14785ef8dfd108353caba59be25
ninja.aoshi <ninja.aoshi@e12224b2-b8a6-a7d7-3dec-a5a3c91f73d2>
2011-08-05 23:58:36 +0000

    Modifiacion de las enfermedades micologicas, ya que anteriormente se habian registrado erroneamente de mi parte otros datos de otra enfermedad.

--- trunk/sql/public/tablas/INSERT/17  enfermedades_micologicas.sql
+++ trunk/sql/public/tablas/INSERT/17  enfermedades_micologicas.sql
@@ -1,32 +1,104 @@
-﻿INSERT INTO enfermedades_micologicas (
+﻿ALTER TABLE enfermedades_micologicas
+	ALTER nom_enf_mic TYPE CHARACTER VARYING(100);
+
+INSERT INTO enfermedades_micologicas (
 	id_enf_mic,
 	nom_enf_mic,
 	id_tip_mic	
 ) VALUES (
 	1,
-	'Actinomicetoma',
+	'Dermatofitosis',
 	1
 ),
 (
 	2,
-	'Eumicetoma',
+	'Onicomicosis dermatofitica',
 	1
 ),
 (
 	3,
-	'Esporotricosis',
+	'Onicomicosis no dermatofitica',
 	1
 ),
 (
 	4,
-	'Cromoblastomicosis',
+	'Petitiriasis vericolor',
 	1
 ),
 (
 	5,
-	'Lobomicosis',
+	'Piedra blanca',
 	1
-);
+),
+(
+	6,
+	'Tiña negra',
+	1
+),
+(
+	7,
+	'Oculomicosis',
+	1
+),
+(
+	8,
+	'Otomicosis',
+	1
+)
+,
+(
+	9,
+	'Tinea capitis',
+	1
+)
+,
+(
+	10,
+	'Tinea barbae',
+	1
+)
+,
+(
+	11,
+	'Tinea corporis',
+	1
+),
+(
+	12,
+	'Tinea cruris',
+	1
+),
+(
+	13,
+	'Tinea imbricata',
+	1
+),
+(
+	14,
+	'Tinea manuum',
+	1
+),
+(
+	15,
+	'Tinea pedis',
+	1
+),
+(
+	16,
+	'Tinea unguium',
+	1
+),
+(
+	17,
+	'Cromomicosis dermatofitica',
+	1
+),
+(
+	18,
+	'Otro',
+	1
+)
+;
 
 ALTER SEQUENCE enfermedades_micologicas_id_enf_mic_seq
-	RESTART 6;
+	RESTART 19;
